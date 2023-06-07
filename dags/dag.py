73adc805191c2786b0c1af37f822eed8b7076510
Papkidon <81909489+Papkidon@users.noreply.github.com>
2023-06-07 07:08:10 +0200

    refactor: change workflow invoking method

--- dags/dag.py
+++ dags/dag.py
@@ -1,26 +1,29 @@
 from datetime import datetime
 
+from airflow.decorators import dag
 from airflow.operators.python import PythonOperator
 from scripts.save import save_to_db
 from scripts.scrape import scrape_from_olx
 
 from airflow import DAG
 
-with DAG(
-    "scrape_items",
-    start_date=datetime(2023, 6, 5),
-    schedule_interval="@hourly",
-    catchup=False,
-) as dag:
-    scrape_items_task = PythonOperator(
-        task_id="scrape",
-        python_callable=scrape_from_olx,
-        op_kwargs={"item": "iphone"},
-    )
+default_args = {"start_date": datetime(2023, 6, 8)}
 
-    save_items_task: PythonOperator = PythonOperator(
-        task_id="save",
-        python_callable=save_to_db,
-    )
 
-    scrape_items_task >> save_items_task
+@dag(schedule="@daily", default_args=default_args, catchup=False)
+def scrape_dag():
+    # scrape_items_task = PythonOperator(
+    #     task_id="scrape",
+    #     python_callable=scrape_from_olx,
+    #     op_kwargs={"item": "iphone"},
+    # )
+
+    # save_items_task: PythonOperator = PythonOperator(
+    #     task_id="save",
+    #     python_callable=save_to_db,
+    # )
+
+    scrape_from_olx(save_to_db())
+
+
+scrape_dag()
